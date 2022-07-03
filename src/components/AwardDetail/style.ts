f1cb5489d663809e2baf84de9148cb41d2b0e725
YeonJae <duswo9595@naver.com>
2022-07-03 22:38:21 +0900

    feat: Award Badge Item (#8)

--- src/components/AwardDetail/style.ts
+++ src/components/AwardDetail/style.ts
@@ -2,4 +2,21 @@ import styled from 'styled-components'
 
 import { ContentContainer } from '../common/Layout'
 
-export const AwardDetailContainer = styled(ContentContainer)``
+export const AwardDetailContainer = styled(ContentContainer)`
+  align-items: flex-start;
+  padding: 130px 50px 135px 100px;
+
+  ul + ul {
+    margin-top: 30px;
+  }
+`
+
+export const IndicatorListContainer = styled.ul``
+
+export const AwardBadgeListContainer = styled.ul`
+  display: flex;
+
+  li + li {
+    margin-left: 39px;
+  }
+`
--- src/components/MatricsItem/style.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const MetricsItemParagraph = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.large};
-  margin-bottom: 20px;
-`
